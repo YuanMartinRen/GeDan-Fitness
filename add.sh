97e3740f3e9525f6ca528ec664b703269c7afdb3
Martin Ren <m01001101@live.com>
2017-03-30 14:01:20 +1100

    Move project from bitbuck to github

---
+++ add.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+git add fitnessteaching/fitnessteaching/urls.py
+git add fitnessteaching/fitness/*.py
+git add fitnessteaching/fitness/templates/*
+git add fitnessteaching/fitness/static/*
+git add fitnessteaching/insert_data/*
+git add fitnessteaching/input_users_videos.sh
+git add fitnessteaching/fitnessteaching/settings.py
